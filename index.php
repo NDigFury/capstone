b5a11ac406bd18684b0c0820dfc2e835bc302f46
StevenWerden <stevenwerden@yahoo.com>
2023-03-16 12:49:26 -0700

    Changed System
    The main file now runs on PHP through XAMPP (localhost) in order to read the csv file.

--- index.php
+++ index.php
@@ -1,12 +1,50 @@
-<?php
-echo "<html><body><table>\n\n";
-$f = fopen("survey.csv", "r");
-while (($line = fgetcsv($f)) !== false) {
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Opportunity System | University of Arizona</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <div class="redRow"></div>
+  <div class="headerMain">
+    <img class="logo" src="https://www.arizona.edu/sites/default/files/www_webheader-01.svg">
+    <ul class="menuItems">
+      <li><a href="https://www.arizona.edu/">Home</a></li>
+      <li><a href="https://www.arizona.edu/about">About</a></li>
+      <li><a href="https://news.arizona.edu/">News</a></li>
+    </ul>
+  </div>
+  <div class="titleContainer">
+    <h1 class="entryTitle">OPPORTUNITY MANAGEMENT SYSTEM</h1>
+  </div>
+  <div class="tableContainer">
+        <table>
+        <thead>
+        <tr>
+        <th><div> Student Name(s) (Last, First) </div></th>
+        <th><div> Project Title </div></th>
+        <th><div> Year </div></th>
+        <th><div> Report URL </div></th>
+        </tr>
+        </thead>
+    <?php
+      echo "<tbody>\n\n";
+      $f = fopen("survey.csv", "r");
+      while (($line = fgetcsv($f)) !== false){ 
         echo "<tr>";
         foreach ($line as $cell) {
-                echo "<td>" . htmlspecialchars($cell) . "</td>";
-        }
-        echo "</tr>\n";
-}
-fclose($f);
-echo "\n</table></body></html>";
+        echo "<td>" . htmlspecialchars($cell) . "</td>";
+      }
+      echo "</tr>\n";
+      }
+      fclose($f);
+      echo "\n</tbody></table>";
+      ?>
+  </div>
+</body>
+
+</html>
