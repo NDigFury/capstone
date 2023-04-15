66c6f89a802f1a805e179a587d57b23ec8242197
Nikki <97368692+NDigFury@users.noreply.github.com>
2023-04-14 17:54:45 -0700

    Update api.php

--- past_survey/api.php
+++ past_survey/api.php
@@ -2,23 +2,21 @@
 $names = $_POST['names'];
 $title = $_POST['title'];
 $description = $_POST['description'];
-$length = $_POST['length'];
-$difficulty = $_POST['difficulty'];
-$studentID = $_POST['studentID'];
+$difficulty = $_POST['url'];
+$studentID = $_POST['netID'];
 $check = $_POST['check'];
 
 $s ="";
 $s .= htmlspecialchars($names) . ",";
 $s .= htmlspecialchars($title) . ",";
 $s .= htmlspecialchars($description) . ",";
-$s .= htmlspecialchars($length) . ",";
-$s .= htmlspecialchars($difficulty) . ",";
+$s .= htmlspecialchars($url) . ",";
 if(strlen(htmlspecialchars($check)) > 0) { // Check is 'on'
-    $s .= htmlspecialchars($studentID) . ",";
+    $s .= htmlspecialchars($netID) . ",";
     $s .= htmlspecialchars($check);
 }
 else {
-    $s .= htmlspecialchars($studentID);
+    $s .= htmlspecialchars($netID);
 }
 $f = fopen("../survey.csv", "a");
 fwrite($f, "\n".$s);
